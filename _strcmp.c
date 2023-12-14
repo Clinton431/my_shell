2b8084ede280ef8425afc878405a5cbebbdb8de5
Josphat Kahihia <josphatkahihia@gmail.com>
2023-12-14 16:34:37 +0300

    Remove getpid

--- _strcmp.c
+++ _strcmp.c
@@ -6,17 +6,33 @@
 * @s2: string 2
 * Return: int that tells num spaces in between, 0 if exactly the same string
 */
-#include <stdbool.h>
-
+/*#include <stdbool.h>*/
 int _strcmp(const char *s1, const char *s2)
 {
-int i = 0;
 
+        while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return ((*s1 == '\0') ? -1 : 1);
+	}
+}
+/*int i = 0;
 while (s1[i] != '\0' && s1[i] == s2[i])
 i++;
-
 if (s2[i] != '\0')
 return (s1[i] - s2[i]);
 else
-return (0);
-}
+return (0);*/
